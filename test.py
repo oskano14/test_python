9edf5982b15fff7eb9b9ff7a83124d610d104be7
oskano14 <gnabrot@gmail.com>
2025-11-12 15:31:59 +0100

    feature = jean de la patrie

--- test.py
+++ test.py
@@ -65,13 +65,13 @@ print(vigere_uncipher(vigenere_cipher(test, "clefsecrete"), "clefsecrete"))
 
 
 
-if __name__ == "__main__":
-# 	message = "le chocolat est bon"
+# if __name__ == "__main__":
+#     # 	message = "le chocolat est bon"
 
-# 	crypted_text = cesar_cipher(message, 12) # exo 1
-# 	print(crypted_text)
+#     # 	crypted_text = cesar_cipher(message, 12) # exo 1
+#     # 	print(crypted_text)
 
-# 	initial_message = cesar_uncipher(crypted_text, 12) # exo 2
-# 	print(initial_message == message)
+#     # 	initial_message = cesar_uncipher(crypted_text, 12) # exo 2
+#     # 	print(initial_message == message)
 
-# 	hack_cesar_cipher(crypted_text, alphabet=string.printable) # exo3
+#     # 	hack_cesar_cipher(crypted_text, alphabet=string.printable) # exo3
